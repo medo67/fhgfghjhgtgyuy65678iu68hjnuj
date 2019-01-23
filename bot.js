5fc17bfd827ecb88f74365e2b268ef9d9eda2596
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-23 21:37:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -660,7 +660,7 @@ m.sendMessage(args)
    }
    });
 
-const developers = ["472413769700474901","id"]
+const developers = ["486701400152342528","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
